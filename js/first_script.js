a42fbccb5f1834c9355fd345b10908076b45cd0b
Yehor Mukhladi <yehor.mukhladi@logianalytics.com>
2022-11-25 19:43:55 +0200

    initial commit for repo

---
+++ js/first_script.js
@@ -0,0 +1,13 @@
+alert('act 1');
+
+//var
+//let
+const money = 10;
+
+alert(money);
+
+// number 112312535
+// text "some text"
+// boolean true | false
+// null
+
---
+++ js/second_script.js
@@ -0,0 +1,6 @@
+alert('act 2');
+
+const name = prompt('What is your name?', 'User');
+
+alert("Hello, " + name + "! How are you?");
+alert(`Hello, ${name}! How are you?`);
